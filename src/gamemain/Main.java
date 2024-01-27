3938251e5bedb92bdbbd1348eb2528e9cc163854
myself54188 <aserg14524>
2024-01-27 20:59:29 +0800

    坦克大战

---
+++ src/gamemain/Main.java
@@ -0,0 +1,12 @@
+package gamemain;
+
+import gameframe.GameFrame;
+
+/**
+ * 程序的入口
+ */
+public class Main {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
---
+++ src/util/Constant.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.awt.*;
+
+/**
+ * 常量类
+ */
+public class Constant {
+    // 游戏窗口名字
+    public static final String GAME_TITLE = "坦克大战";
+
+    // 游戏窗口大小
+    public static final int FRAME_WIDTH = 800;
+    public static final int FRAME_HEIGHT = 600;
+
+    // 游戏菜单状态
+    public static final int STATE_MENU = 0;
+    public static final int STATE_HELP = 1;
+    public static final int STATE_ABOUT = 2;
+    public static final int STATE_RUN = 3;
+    public static final int STATE_OVER = 4;
+
+    // 游戏菜单界面文字
+    public static final String[] MENUS = {
+            "开始游戏",
+            "继续游戏",
+            "游戏帮助",
+            "关于游戏",
+            "退出游戏"
+    };
+
+    // 游戏菜单字体
+    public static final Font GAME_FONT = new Font("宋体", Font.BOLD, 24);
+}
